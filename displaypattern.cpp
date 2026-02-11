c3224dd043b0ce92126f27a51fce1c55d2b10e3d
unknown <nirvansamman@gmail.com>
2026-02-11 10:21:55 +0530

    commit 7

---
+++ displaypattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    for (int i= 1; i<= N; i++) {
+        for (int s= 1; s<= N-i; s++) {
+            cout<<" ";
+        }
+        for (int j= 1; j<= i; j++) {
+            cout<<j;
+        }
+        for (int j= i-1; j>= 1; j--) {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
