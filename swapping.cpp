359e143239a69651e29922eaa9959d170b2ca2c6
unknown <nirvansamman@gmail.com>
2026-02-11 13:56:07 +0530

    commit 19

---
+++ swapping.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter the digits:";
+    cin>>a>>b;
+    cout<<"before swapping numbers":a="a<<",B="b<<"endl;
+    return 0;
+}
